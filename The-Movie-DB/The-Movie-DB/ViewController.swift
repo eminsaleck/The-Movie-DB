39249dc8c5bc4806e9436507a446d74bf55c3a94
username <email@blahblah.com>
2022-10-20 08:44:52 +0300

    Initial commit

---
+++ The-Movie-DB/The-Movie-DB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  The-Movie-DB
+//
+//  Created by LEMIN DAHOVICH on 20.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
