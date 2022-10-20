656d98194356eaa1288a388471da134ea9d90620
username <email@blahblah.com>
2022-10-20 09:03:09 +0300

    Commit

--- The-Movie-DB/The-Movie-DB/ViewController.swift
+++ The-Movie-DB/The-Movie-DB/ViewController.swift
@@ -7,6 +7,8 @@
 
 //Pagination
 
+//selectedItem ->   var data: Displayable? (DetailVC)
+
 
 import UIKit
 import Alamofire
@@ -28,7 +30,7 @@ class ViewController: UIViewController {
 
 
 extension ViewController{
-func fetchFilms() {
+private func fetchFilms() {
     let url = "https://api.themoviedb.org/3/movie/popular?api_key=a5ac3411803536cfb4b1cd90557dc8a7&language=en-US&page=1"
     
     AF.request(url, method: .get)
@@ -42,3 +44,43 @@ func fetchFilms() {
         }
 }
 }
+
+
+/*
+
+ // MARK: - Alamofire
+ extension DetailViewController {
+   private func fetch<T: Decodable & Displayable>(_ list: [String], of: T.Type) {
+     var items: [T] = []
+     let fetchGroup = DispatchGroup()
+     
+     list.forEach { (url) in
+       fetchGroup.enter()
+       AF.request(url).validate().responseDecodable(of: T.self) { (response) in
+         if let value = response.value {
+           items.append(value)
+         }
+         fetchGroup.leave()
+       }
+     }
+     
+     fetchGroup.notify(queue: .main) {
+       self.listData = items
+       self.listTableView.reloadData()
+     }
+   }
+   
+   func fetchList() {
+     guard let data = data else { return }
+     
+     switch data {
+     case is Film:
+       fetch(data.listItems, of: Starship.self)
+     case is Starship:
+       fetch(data.listItems, of: Film.self)
+     default:
+       print("Unknown type: ", String(describing: type(of: data)))
+     }
+   }
+ }
+*/
