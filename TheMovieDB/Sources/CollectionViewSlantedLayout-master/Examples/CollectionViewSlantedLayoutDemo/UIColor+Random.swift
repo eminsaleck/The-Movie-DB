6d52975af4fe38bd2f8e07b4420529ab062bfeda
Emin S <74063331+eminsaleck@users.noreply.github.com>
2022-12-14 11:31:31 +0200

    Delete TheMovieDB/Sources/CollectionViewSlantedLayout-master directory

--- TheMovieDB/Sources/CollectionViewSlantedLayout-master/Examples/CollectionViewSlantedLayoutDemo/UIColor+Random.swift
+++
@@ -1,9 +0,0 @@
-//
-//  UIColor+Random.swift
-//  YBSlantedCollectionViewLayoutSample
-//
-//  Created by Yassir Barchi on 30/12/2017.
-//  Copyright © 2017 Yassir Barchi. All rights reserved.
-//
-
-import Foundation
